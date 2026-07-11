f627c90980116507ac44c10b8ebce12b06d780fa
Harris Rauf <harisrauf04@gmail.com>
2026-07-12 00:25:04 +0500

    fix(sim): restore Memory class + compute_memory_prompt removed in tech-lead merge

--- simulation/events/__init__.py
+++ simulation/events/__init__.py
@@ -9,14 +9,12 @@ from simulation.events.event_bus import EventBus
 
 # Media engine
 from simulation.events.media_engine import (
-    MediaState,
     NewsArticle,
     process_media_tick,
 )
 
 __all__ = [
     "EventBus",
-    "MediaState",
     "NewsArticle",
     "process_media_tick",
 ]
